4740685b534c213c3023364837ff3fc5e171516e
Solovey Roman <120035099+ByeLarry@users.noreply.github.com>
2023-05-22 22:36:57 +0300

    some changes

---
+++ lab6/pch.hpp
@@ -0,0 +1,10 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+//макросы для упрощения разработки 
+#define RUS setlocale(LC_ALL, "Russian")
+#define PRINT cout << 
+#define INPUT  cin >> 
